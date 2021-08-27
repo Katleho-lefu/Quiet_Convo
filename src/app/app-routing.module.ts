3b5784d21595dbea36034cf2f7ed1231f40c83de
Victor Lefu <victor@thedigitalacademy.co.za>
2021-08-27 09:26:23 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+
+const routes: Routes = [
+  {
+    path: 'landing',
+    component: LandingComponent
+  },
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
